2a1f5cf2da83334928a0f8b749af2e485dd3da2d
Danila Gunkov <dangun711@gmail.com>
2024-11-20 20:12:57 +0300

    TUNED CLASS BOOK
    create new methods
    _get_all_json
    on create class make get all json in fiels
    
    _chenges
    make chenges status in all_json
    
    _save
    save_all_chenges
    
    addbook
    create new book
    
    cheng_status_book

--- main.py
+++ main.py
@@ -1,30 +1,58 @@
 import json
+
+STATUS_CHOISES = {
+    '1':'В наличии',
+    '2':'Выданна',
+}
+
 class Book:
+    def __init__(self):
+        self.all_json = self._get_all_json()
+
+    def _get_all_json(self):
+        with open('books.json','r') as a:
+            return json.load(a)
+        
+    def _chenges(self, data_for_chages_book):
+        try:
+            book = self.all_json[data_for_chages_book]
+            type_status = input('Для изменения статуса на \'в наличии\' введите 1 \n'
+                                'для изменения статуса на \'выданна\' ddtlbnt 2\n'
+                                'Введите операцию: ')
+            if type_status == '1':
+                book['statys'] = STATUS_CHOISES[type_status]
+            elif type_status == '2':
+                book['statys'] = STATUS_CHOISES[type_status]
+            self._save()
+        except KeyError:
+            print('Книга не найдена,  пожалуйста проверьте вводимые данные')
+
+    def _save(self):
+        print(self.all_json)
+        with open('books.json', 'w') as add:
+            json.dump(self.all_json, add)
+        
     def addbook(self):
         title = input('Введите название книги: ')
-        Aythor = input('Введите имя и фамилию автора: ')
+        aythor = input('Введите имя и фамилию автора: ')
         year = input('Введите дату издания: ')
-        json_data = main()
-        with open('books.json', 'w'):
-            pass
+        self.all_json[3] = {'title':title, 'aythor':aythor, 'year':year, 'statys':'выдана'}
+        self._save()
+            
 
-def main(author_name):
-    with open('books.json','r') as a:
-        return json.load(a)
-def changes():
-    book_for_changes = input('Введите имя и фамилию автора для изменения')
-    read_json = main(author_name = book_for_changes)
-    with open('books.json', 'w') as s:
-        read_json['dcs'] = 1
-        print(json.dump(read_json, s))
-
-x = {
-    'books':{
-        1:{
-            'title':'fvdsv',
-            'Author':'cedds',
-            'year':'vdscv',
-            'statys':'cscedws'
-        }
-    }
-}
+    def cheng_status_book(self):
+        data_for_chages_book = input('Введите id, название, автора, или дату издания книги: ')
+        if data_for_chages_book.isdigit():
+            self._chenges(data_for_chages_book)
+        else:
+            for id_book in self.all_json:
+                info_book = self.all_json[id_book]
+                for i_f in info_book:
+                    if info_book[i_f] == data_for_chages_book:
+                        self._chenges(id_book)
+        # with open('books.json', 'w') as s:
+        #     read_json['dcs'] = 1
+        #     print(json.dump(read_json, s))
+if __name__ == '__main__':
+    x = Book()
+    print(x.all_json)
